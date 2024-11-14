b3566b6f77a2b282ffa54958e605de62d915a3c4
Daniel-derf <danielpardinho321@gmail.com>
2024-11-14 20:03:16 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { useState } from 'react'
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import './App.css'
+
+// components
+import Navbar from './components/Navbar'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
 
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {}
+
+const About = (props: Props) => {
+  return (
+  <>
+    <h1>About</h1>
+    
+  
+  
+  
+  </>
+  )
+}
+
+export default About
