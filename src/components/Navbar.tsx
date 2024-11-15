e4e51a302b923ede7c0adeaaf898430646102e6e
Daniel-derf <danielpardinho321@gmail.com>
2024-11-15 20:10:02 -0300

    style: navbar initial style

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import './Navbar.css'
+import iconImg from '../assets/images/flat-vintage-car-icon-8bd3aa.png'
 import { NavLink } from 'react-router-dom'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
   return (
-  <nav>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/about">About</NavLink>
+  <nav className='navbar'>
+    <NavLink to="/"><img className='navbar-icon' src={iconImg} alt="" /></NavLink>
+
+    <div className='navbar-options'>
+      <NavLink to="/">Início</NavLink>
+      <NavLink to="/about">Sobre</NavLink>
+    </div>
+
   </nav>
   )
 }
